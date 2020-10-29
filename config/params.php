694195826ca4e1de7e14788ee8f3afe293b93269
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2020-10-29 07:13:34 -0300

    Remove unnecesary aliases params. (#39)

--- config/params.php
+++ config/params.php
@@ -3,15 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'aliases' => [
-        '@root' => dirname(__DIR__, 1),
-        '@asset' => '@root/tests/public/assets',
-        '@assetUrl'  => '/baseUrl',
-        '@converter' => '@root/tests/public/assetconverter',
-        '@npm' => '@root/node_modules',
-        '@testSourcePath' => '@root/tests/public/assetsources'
-    ],
-
     'yiisoft/asset' => [
         'assetConverter' => [
             'command' => [
