22d2910f2233e890794890af0740378c97e9a0b4
Dmitrii Derepko <xepozz@list.ru>
2023-03-27 12:14:11 +0300

    Use exported abstract class

--- tests/Debug/AssetCollectorTest.php
+++ tests/Debug/AssetCollectorTest.php
@@ -7,7 +7,7 @@ namespace Yiisoft\Assets\Tests\Debug;
 use Yiisoft\Assets\AssetBundle;
 use Yiisoft\Assets\Debug\AssetCollector;
 use Yiisoft\Yii\Debug\Collector\CollectorInterface;
-use Yiisoft\Yii\Debug\Tests\Collector\AbstractCollectorTestCase;
+use Yiisoft\Yii\Debug\Tests\Shared\AbstractCollectorTestCase;
 
 final class AssetCollectorTest extends AbstractCollectorTestCase
 {
