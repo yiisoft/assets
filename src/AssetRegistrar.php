368c838bb07bb801e407402d1b74028833d2fef3
Дмитрий <Gerych84@gmail.com>
2026-01-20 20:31:10 +0300

    Resolve conflicts

--- src/AssetRegistrar.php
+++ src/AssetRegistrar.php
@@ -26,6 +26,7 @@ use function sprintf;
  * @psalm-import-type JsString from AssetManager
  * @psalm-import-type JsVar from AssetManager
  * @psalm-import-type ConverterOptions from AssetConverterInterface
+ * @psalm-import-type Imports from AssetManager
  * @psalm-type ImportModule = array{0: string}|array<string, string>
  */
 final class AssetRegistrar
@@ -57,6 +58,9 @@ final class AssetRegistrar
      */
     private array $jsVars = [];
 
+    /**
+     * @psalm-var Imports
+     */
     private array $imports = [
         'imports' => [],
         'integrity' => [],
@@ -527,7 +531,6 @@ final class AssetRegistrar
             $key = $module;
         }
 
-        /** @var string $key */
         if (isset($this->imports['imports'][$key])) {
             throw new InvalidConfigException('Module name should be a unique.');
         }
