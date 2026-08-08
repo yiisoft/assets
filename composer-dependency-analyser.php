516af0ef70cdb6dccb5b0c0bfab0ecde7bea9e78
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-08 15:16:37 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -11,6 +11,4 @@ return (new Configuration())
     ->addPathToScan(__DIR__ . '/config', isDev: false)
     ->addPathToScan(__DIR__ . '/src', isDev: false)
     ->addPathToScan(__DIR__ . '/tests', isDev: true)
-    // AssetCollector optionally integrates with yiisoft/yii-debug without requiring it at runtime;
-    // previously whitelisted the same way in composer-require-checker.json.
     ->ignoreErrorsOnPackage('yiisoft/yii-debug', [ErrorType::DEV_DEPENDENCY_IN_PROD]);
